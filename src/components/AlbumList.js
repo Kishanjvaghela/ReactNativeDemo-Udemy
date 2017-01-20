b86502154d7b9cdd030f16999f296954063d3b01
Kishan Vaghela <kishan.j.vaghela@gmail.com>
2017-01-20 17:43:50 +0530

    section 7 continue ...

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
-      console.log('componentWillMount in AlbumList');    
+      console.log('componentWillMount in AlbumList');
+      const URL = 'https://rallycoding.herokuapp.com/api/music_albums';
+      fetch(URL)
+        .then(response => response.json())
+        .then((data) => {
+          this.setState({ albums: data });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail
+        key={album.title}
+        album={album}
+      />
+    );
   }
 
   render() {
+    console.log('its render method');
     return (
         <View>
-            <Text>Album list ...</Text>
+            {this.renderAlbums()}
         </View>
     );
   }
